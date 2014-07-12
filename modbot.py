ef5273359b951693dff656f7131ca0fa63ad40a4
markekraus <github@markekraus.com>
2014-07-12 16:47:02 -0500

    Verison 2.0
    Major update, lots of changes
    
    * Added a "top 100" check to see if the video is a repost (only grabs the top of all time 100 list once a day)
    * Added a repost check for submissions in the last three months
    * Change the logic around quite a bit to reduce redundant code and to better handle exceptions
    * Added the ability to parse Youtube video ID's from "attribution_link" style URL's
    * Now that we are confident in our URL checks, added functionality to remove posts with non-compliant URL's
    * Added more API compliance steps so the bot plays nice. It probably plays too nice now as initial loop takes several minutes to complete with all the sleeps before calling searches.
    
    We still haven't discussed self-posts so for now they are just a screen alert and nothing more.

--- modbot.py
+++ modbot.py
@@ -20,86 +20,157 @@ yt_service.ProgrammaticLogin()
 already_done = []
 
 def video_id(value):
-    """
-    Examples:
-    - http://youtu.be/SA2iWivDJiE
-    - http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
-    - http://www.youtube.com/embed/SA2iWivDJiE
-    - http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
-    """
-    query = urlparse(value)
-    if query.hostname == 'youtu.be':
-        return query.path[1:]
-    if query.hostname in ('www.youtube.com', 'youtube.com'):
-        if query.path == '/watch':
-            p = parse_qs(query.query)
-            return p['v'][0]
-        if query.path[:7] == '/embed/':
-            return query.path.split('/')[2]
-        if query.path[:3] == '/v/':
-            return query.path.split('/')[2]
-    # fail?
-    return None
+	"""
+	Examples:
+	- http://youtu.be/SA2iWivDJiE
+	- http://www.youtube.com/watch?v=_oPAwA_Udwc&feature=feedu
+	- http://www.youtube.com/attribution_link?a=AbE6fYtNaa4&u=%2Fwatch%3Fv%3DNbyHNASFi6U%26feature%3Dshare
+	- http://www.youtube.com/embed/SA2iWivDJiE
+	- http://www.youtube.com/v/SA2iWivDJiE?version=3&amp;hl=en_US
+	"""
+	query = urlparse(value)
+	if query.hostname == 'youtu.be':
+		return query.path[1:]
+	if query.hostname in ('www.youtube.com', 'youtube.com'):
+		if query.path == '/watch':
+			p = parse_qs(query.query)
+			return p['v'][0]
+		if query.path == '/attribution_link':
+			p = parse_qs(query.query)
+			p = urlparse(p['u'][0])
+			p = parse_qs(p.query)
+			return p['v'][0]
+		if 1 == 1:
+			print 'WTF??'
+		if query.path[:7] == '/embed/':
+			return query.path.split('/')[2]
+		if query.path[:3] == '/v/':
+			return query.path.split('/')[2]
+	# fail?
+	return None
 	
-r = praw.Reddit('PRAW /r/deepintoyoutube modbot by /u/markekraus 1.0. '
+r = praw.Reddit('PRAW /r/deepintoyoutube modbot by /u/markekraus 2.0. '
 				'URL: https://github.com/markekraus/DeepIntoYouTubemodBot')
 r.login()
+lasttopget = 0
+topsubmissionvids = []
+
 while True:
 	loopstart = time.time()
 	subreddit = r.get_subreddit('deepintoyoutube')
-	for submission in subreddit.get_new(limit=100):
-		problem = 0
-		if submission.id not in already_done:
-			if submission.is_self:
-				print '!!! Selfpost !!!!'
-				print 'Permalink:' 
-				pprint(submission.permalink)
-				print ''
-				already_done.append(submission.id)
-				break
-			else:
-				suburl = urlparse(submission.url)
-				if any(suburl.hostname in s for s in yt_hostnames):
-					suburlqs = parse_qs(suburl.query)
-					try:
-						entry = yt_service.GetYouTubeVideoEntry(video_id=video_id(submission.url))
-					except:
-						#print 'poorly formated URL'
-						#pprint(submission.url)
-						#pprint(submission.permalink)
-						#pprint(suburl.query)
-						break
-					_tmp = time.strptime(entry.published.text, '%Y-%m-%dT%H:%M:%S.000Z')
-					ptime = datetime.datetime(*_tmp[:6])
-					now = datetime.datetime.now()
-					tdelta = now - ptime
-					seconds = tdelta.total_seconds()
-					if seconds < 12360000:
-						print '!!! Video is newer than 4.7 months !!!!'
-						modcomment = submission.add_comment('Your submission violates rule #1, no videos uploaded to YouTube in the past 5 months are allowed. Your post has been removed automatically. If you believe it has been removed in error, please [message the moderators](http://www.reddit.com/message/compose?to=%2Fr%2FDeepIntoYouTube).')
-						problem = 1
-					if hasattr(entry.statistics, 'view_count') and float(entry.statistics.view_count) > 200000:
-						print '!!! Video has been viewed more than 200000 times !!!!'
-						modcomment = submission.add_comment('Your submission violates rule #4, no YouTube videos with greater than 200,000 views are allowed. Your post has been removed automatically. If you believe it has been removed in error, please [message the moderators](http://www.reddit.com/message/compose?to=%2Fr%2FDeepIntoYouTube).')
-						problem = 1
+	if lasttopget < loopstart - 86400:
+		print 'Getting top 100 submissions...'
+		try:
+			topsubmissions = subreddit.get_top_from_all(limit=100)
+		except:
+			print '** Failed to get top sumissions'
+			print ''
+		else:
+			lasttopget = time.time()
+			topsubmissionvids = []
+			for topsubmission in topsubmissions:
+				try:
+					topytvid=video_id(topsubmission.url)
+				except:
+					print
 				else:
-					print '!!! Not a YouTube link !!!!'
-					modcomment = submission.add_comment('Your submission does not appear to contain a link to YouTube. Your post has been removed automatically. If you believe it has been removed in error, please [message the moderators](http://www.reddit.com/message/compose?to=%2Fr%2FDeepIntoYouTube).')
-					problem = 1
-				if problem == 1:
-					pprint(submission.url)
-					pprint(suburl.hostname)
-					pprint(seconds)
-					print 'Video published on: %s ' % entry.published.text
-					print 'Video view count: %s' % entry.statistics.view_count
+					if topytvid != None:
+						topsubmissionvids.append(topytvid)
+			pprint(topsubmissionvids)
+			print 'Top submissions grabbed!'
+			print ''
+		sleepfor = max(0.0, 30.0 - (time.time() - loopstart))
+		time.sleep(sleepfor)
+	try:
+		submissions = subreddit.get_new(limit=100)
+	except:
+		e = sys.exc_info()[0]
+		print '**get_new failed: %s' % str(e)
+		print 'time: ' + time.strftime("%c")
+		print ''
+	else:
+		for submission in submissions:
+			reasons = []
+			if submission.id not in already_done:
+				if submission.is_self:
+					print '!!! Selfpost !!!!'
 					print 'Permalink:' 
 					pprint(submission.permalink)
-					pprint(submission.hidden)
-					modcomment.distinguish(as_made_by='mod')
-					submission.remove(spam=False)
-					print 'Submission removed!'
-					print ''							
-			already_done.append(submission.id)
+					print ''
+				else:
+					suburl = urlparse(submission.url)
+					if any(suburl.hostname in s for s in yt_hostnames):
+						try:
+							ytvid=video_id(submission.url)
+						except:
+							print '!!!! poorly formated URL !!!'
+							reasons.append('* The URL you submitted appears to be poorly formated.')
+							print ''
+						else:
+							if ytvid in topsubmissionvids:
+								print '!!!! Repost of a top 100 submission !!!'
+								reasons.append('* This video is in the [top 100](http://www.reddit.com/r/DeepIntoYouTube/top/?sort=top&t=all) submission of all time in this sub. ')
+							try:
+								sleepfor = max(0.0, 30.0 - (time.time() - loopstart))
+								time.sleep(sleepfor)
+								searchres = list(r.search('url:"%s"' % str(ytvid) ,  subreddit='deepintoyoutube'))
+							except:
+								e = sys.exc_info()[0]
+								print '**Search failed! %s' % str(e)
+								print 'time: ' + time.strftime("%c")
+								print ''
+							else:
+								if len(list(searchres)) > 1:
+									tma = time.time() - 7889230
+									for curres in searchres:
+										if curres.id != submission.id and curres.created_utc > tma:
+											print '!!!! Repost !!!'
+											print 'previous post:'
+											pprint(curres.url)
+											pprint(curres.permalink)
+											reasons.append("* This video has [already been posted in the last 3 months](http://www.reddit.com/r/DeepIntoYouTube/search?q=url%3A%22" + str(ytvid) + "%22&restrict_sr=on).")
+											break
+							try:
+								entry = yt_service.GetYouTubeVideoEntry(video_id=ytvid)
+							except:
+								print '**Youtube look up for %s failed!' % str(ytvid)
+								pprint(submission.url)
+								pprint(submission.permalink)
+								print 'time: ' + time.strftime("%c")
+								print ''
+							else:
+								_tmp = time.strptime(entry.published.text, '%Y-%m-%dT%H:%M:%S.000Z')
+								ptime = datetime.datetime(*_tmp[:6])
+								now = datetime.datetime.now()
+								tdelta = now - ptime
+								seconds = tdelta.total_seconds()
+								if seconds < 12360000:
+									print '!!! Video is newer than 4.7 months !!!!'
+									reasons.append('* Your submission violates rule #1, no videos uploaded to YouTube in the past 5 months are allowed. ')
+								if hasattr(entry.statistics, 'view_count') and float(entry.statistics.view_count) > 200000:
+									print '!!! Video has been viewed more than 200000 times !!!!'
+									reasons.append('* Your submission violates rule #4, no YouTube videos with greater than 200,000 views are allowed. ')
+					else:
+						print '!!!! Submission does not contain valid youtube link !!!'
+						reasons.append('* Your submission does not appear to contain a link to YouTube.')
+					if len(reasons) > 0:
+						modcommenttxt = "Your submission has been automatically removed for the following reason(s):\n\n"
+						for reason in reasons:
+							modcommenttxt += str(reason) + "\n\n"
+						modcommenttxt += "\n\nIf you believe it has been removed in error, please [message the moderators](http://www.reddit.com/message/compose?to=%2Fr%2FDeepIntoYouTube)."
+						pprint(submission.url)
+						pprint(submission.permalink)
+						pprint(submission.author)
+						print 'Video published on: %s ' % entry.published.text
+						if hasattr(entry.statistics, 'view_count'):
+							print 'Video view count: %s' % entry.statistics.view_count
+						modcomment = submission.add_comment(modcommenttxt)
+						modcomment.distinguish(as_made_by='mod')
+						submission.remove(spam=False)
+						print 'Submission removed!'
+						print 'time: ' + time.strftime("%c")
+						print ''
+				already_done.append(submission.id)
 	loopend = time.time()
 	sleepfor = max(0.0, 30.0 - (loopend - loopstart))
 	time.sleep(sleepfor)
